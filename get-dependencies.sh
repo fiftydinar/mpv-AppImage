af5c710857f453d6e6ae233f181214f3162ea548
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-08-24 03:18:35 -0400

    add png encoder
    #49

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -69,6 +69,7 @@ echo "--disable-librsvg"       >> ./ffmpeg_options
 echo "--disable-programs"      >> ./ffmpeg_options
 echo "--disable-encoders"      >> ./ffmpeg_options
 echo "--enable-encoder=rawvideo" >> ./ffmpeg_options
+echo "--enable-encoder=png"    >> ./ffmpeg_options
 echo "--disable-muxers"        >> ./ffmpeg_options
 echo "--enable-muxer=mp4"      >> ./ffmpeg_options
 echo "--enable-muxer=matroska" >> ./ffmpeg_options
